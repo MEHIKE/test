f56b92750a0b964864fa86ecd753ffade3ecefce
Rünno Ruul <rynno.ruul@telia.ee>
2018-09-18 21:53:02 +0300

    deleted repo

--- src/main/java/eu/babywatcher/services/test/model/repo/MyTestRepository.java
+++ src/main/java/eu/babywatcher/services/test/model/repo/MyTestRepository.java
@@ -2,7 +2,7 @@ package eu.babywatcher.services.test.model.repo;
 
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.data.mongodb.repository.MongoRepository;
 
 import eu.babywatcher.services.test.mongo.domain.MyTest;
 
