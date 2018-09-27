ce100daa76bb79ea15c7331ddc2f3d3055c8e71f
Rünno Ruul <rynno.ruul@telia.ee>
2018-09-27 10:34:44 +0300

    c

--- src/main/java/eu/babywatcher/services/test/Postgres2DbConfig.java
+++ src/main/java/eu/babywatcher/services/test/Postgres2DbConfig.java
@@ -23,7 +23,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.google.common.base.Preconditions;
+//import com.google.common.base.Preconditions;
 
 /*@Configuration
 @EnableTransactionManagement
