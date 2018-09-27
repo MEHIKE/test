77a545dc187a79e7872a62787c0e31bbd376bcfa
Rünno Ruul <rynno.ruul@telia.ee>
2018-09-27 07:48:07 +0300

    c

--- src/main/java/eu/babywatcher/services/test/MyTestApplication.java
+++ src/main/java/eu/babywatcher/services/test/MyTestApplication.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -25,11 +27,13 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+//@EnableEurekaClient
+@EnableAutoConfiguration
 //@EnableMongoRepositories
 @EnableSwagger2
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class MyTestApplication {
 
 	  @Autowired
